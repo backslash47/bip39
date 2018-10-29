5cd8278f4ff9a5ee9baf78752a4ac5ac99c2720f
Matus Zamborsky <zamborsky@gmail.com>
2018-10-29 14:21:37 +0100

    formatted worlist script

--- wordlist.ts
+++ wordlist.ts
@@ -1,22 +1,21 @@
-import axios from "axios";
-import { log } from "console";
-import { writeFile } from "fs";
-import { promisify } from "util";
+import axios from 'axios';
+import { log } from 'console';
+import { writeFile } from 'fs';
+import { promisify } from 'util';
 const write = promisify(writeFile);
 
 async function getWordsList() {
-  const url: string =
-    "https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/";
+  const url: string = 'https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/';
 
   const words: string[] = [
-    "chinese_simplified",
-    "chinese_traditional",
-    "english",
-    "french",
-    "italian",
-    "japanese",
-    "korean",
-    "spanish"
+    'chinese_simplified',
+    'chinese_traditional',
+    'english',
+    'french',
+    'italian',
+    'japanese',
+    'korean',
+    'spanish'
   ];
 
   const getter: Array<{ type: string; url: string }> = [];
@@ -25,7 +24,7 @@ async function getWordsList() {
     getter.push({ type: word, url: `${url}${word}.txt` });
   }
 
-  const res = getter.map(async v => {
+  const res = getter.map(async (v) => {
     const tmp = await axios.get(v.url);
     return {
       data: tmp.data as string,
@@ -35,12 +34,8 @@ async function getWordsList() {
 
   for (const i of res) {
     const { type, data } = await i;
-    await write(
-      `src/wordlist/${type}.json`,
-      JSON.stringify(data.split("\n")),
-      { flag: "w" }
-    );
+    await write(`src/wordlist/${type}.json`, JSON.stringify(data.split('\n')), { flag: 'w' });
   }
 }
 
-getWordsList().catch(err => log(err.message));
+getWordsList().catch((err) => log(err.message));
